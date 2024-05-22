44463052086c4fd547bf02e222302bf3eadeedc9
Kashyap Sureshchandra Patel <kashyappatel.research@gmail.com>
2024-05-22 23:24:55 +0530

    Encapsulation

---
+++ data-hiding/encapsulation.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, userName=None, password=None):
+        self.__userName = userName
+        self.__password = password
+
+    def login(self, userName, password):
+        if ((self.__userName.lower() == userName.lower())
+                and (self.__password == password)):
+            print(
+                "Access Granted against username:",
+                self.__userName.lower(),
+                "and password:",
+                self.__password)
+        else:
+            print("Invalid Credentials!")
+
+
+Steve = User("Steve", "12345")
+Steve.login("steve", "12345")
+
+Steve.login("steve", "6789")
+# Steve.__password
